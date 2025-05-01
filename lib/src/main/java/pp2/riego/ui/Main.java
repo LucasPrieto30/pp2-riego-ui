45e0773fd36a07883587d1fe7a0ef237ab4a50be
LucasPrieto30 <lucasprieto30@gmail.com>
2025-05-01 15:39:03 -0300

    adapto inicio a archivo de configuracion

--- lib/src/main/java/pp2/riego/ui/Main.java
+++ lib/src/main/java/pp2/riego/ui/Main.java
@@ -6,7 +6,7 @@ import com.riego.SmartWaterFactory;
 
 public class Main {
     public static void main(String[] args) {
-    	SmartWater smartWater = SmartWaterFactory.crear("plugins/", "plugins/config/umbrales.json");
+    	SmartWater smartWater = SmartWaterFactory.crear("plugins/config/config.json");
         
         SwingUtilities.invokeLater(() -> new RiegoUI(smartWater));
     }
